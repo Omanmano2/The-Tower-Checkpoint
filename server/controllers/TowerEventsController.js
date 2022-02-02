1835ab208ca5893349b222fff6bd80e1f7714bb6
Owen Manmano <Bigowen27@gmail.com>
2022-02-01 17:41:24 -0700

    Push Server

--- server/controllers/TowerEventsController.js
+++ server/controllers/TowerEventsController.js
@@ -1,6 +1,8 @@
 import { Auth0Provider } from "@bcwdev/auth0provider"
 import BaseController from "../utils/BaseController"
 import { towerEventsService } from "../services/TowerEventsService"
+import { commentsService } from "../services/CommentsService"
+import { attendeesService } from "../services/AttendeesService"
 
 export class TowerEventsController extends BaseController {
   constructor() {
@@ -8,10 +10,13 @@ export class TowerEventsController extends BaseController {
     this.router
       .get('', this.getAll)
       .get('/:eventId', this.getById)
+      .get('/:eventId/comments', this.getCommentsById)
+      .get('/:eventId/attendees', this.getAttendeesByEvent)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
       .delete('/:eventId', this.remove)
       .put('/:eventId', this.edit)
+
   }
 
   async create(req, res, next) {
@@ -56,6 +61,25 @@ export class TowerEventsController extends BaseController {
     try {
       const cancelledEvent = await towerEventsService.removeTowerEvent(req.params.eventId, req.userInfo.id)
       return res.send(cancelledEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getCommentsById(req, res, next) {
+    try {
+      const comments = await commentsService.getCommentByEventId(req.params.eventId)
+      return res.send(comments)
+    } catch (error) {
+      next(error)
+    }
+
+  }
+
+  async getAttendeesByEvent(req, res, next) {
+    try {
+      const attendees = await attendeesService.getAttendenceForEvent(req.params.eventId)
+      return res.send(attendees)
     } catch (error) {
       next(error)
     }
